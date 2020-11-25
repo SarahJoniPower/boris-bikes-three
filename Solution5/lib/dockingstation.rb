e36bf81413a75f55556bab8095020c8d9cb3d6d8
Aman Tank <AmanTank187@gmail.com>
2020-11-25 16:15:04 +0000

    Bike class tests

--- Solution5/lib/dockingstation.rb
+++ Solution5/lib/dockingstation.rb
@@ -1,5 +1,6 @@
+require_relative '../lib/bike.rb'
 class DockingStation
   def release_bike
-
+    bike = Bike.new 
   end
 end
--- Solution5/spec/docking_station_spec.rb
+++ Solution5/spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'dockingstation'
 
 describe DockingStation do
-  it {is_expected.to respond_to(:release_bike)}
+ let(:bike) {Bike.new}
+  describe '#release_bike' do
+    it {is_expected.to respond_to(:release_bike)}
+    it 'It should show true' do
+      expect(bike.working?).to eq true
+    end 
+  end
 end
