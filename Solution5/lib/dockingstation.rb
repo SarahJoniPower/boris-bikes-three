e711fc5799f40069a93cf2fc3a441251d8416905
Ara <hello.ara@pm.me>
2020-11-26 01:36:56 +0900

    add #dock test

--- Solution5/lib/dockingstation.rb
+++ Solution5/lib/dockingstation.rb
@@ -1,6 +1,13 @@
 require_relative '../lib/bike.rb'
 class DockingStation
+ attr_reader :bike
+ attr_accessor :docked_bikes
+
   def release_bike
-    bike = Bike.new 
+    @bike = Bike.new
+  end
+
+  def docking_bikes
+
   end
 end
--- Solution5/spec/docking_station_spec.rb
+++ Solution5/spec/docking_station_spec.rb
@@ -2,10 +2,17 @@ require 'dockingstation'
 
 describe DockingStation do
  let(:bike) {Bike.new}
+
   describe '#release_bike' do
     it {is_expected.to respond_to(:release_bike)}
     it 'It should show true' do
       expect(bike.working?).to eq true
-    end 
+    end
+  end
+
+  describe '#dock' do
+    it 'should dock a bike' do
+      expect(bike.dock).to eq true
+    end
   end
 end
