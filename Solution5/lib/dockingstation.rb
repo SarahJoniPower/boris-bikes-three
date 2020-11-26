301ab3a4f0b378e37ea906d5b873d373a2d7aac0
Ara <hello.ara@pm.me>
2020-11-26 21:17:07 +0900

    update release_bike method

--- Solution5/lib/dockingstation.rb
+++ Solution5/lib/dockingstation.rb
@@ -8,6 +8,9 @@ class DockingStation
 
   def release_bike
     @bike = Bike.new
+    if docked_bikes.count == 0
+      raise StandardError.new('no bikes available')
+    end
   end
 
   def dock(bike)
--- Solution5/spec/docking_station_spec.rb
+++ Solution5/spec/docking_station_spec.rb
@@ -1,17 +1,18 @@
 require 'dockingstation'
 
 describe DockingStation do
- let(:bike) {Bike.new}
- let(:docking_station) {DockingStation.new}
+ let(:bike) { Bike.new }
+ let(:docking_station) { DockingStation.new }
 
   describe '#release_bike' do
-    it {is_expected.to respond_to(:release_bike)}
+    it { is_expected.to respond_to(:release_bike) }
     it 'It should show true' do
       expect(bike.working?).to eq true
     end
 
-    it 'raises "no bikes available"' do
-      expect { @docked_bikes.empty? }.to raise_error("no bikes!")
+    it 'raises an error message when there are no bikes' do
+      docked_bikes = []
+      expect{ docking_station.release_bike }.to raise_error('no bikes available')
     end
   end
 
